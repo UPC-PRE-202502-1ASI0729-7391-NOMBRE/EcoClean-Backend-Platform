51fe43d18d5779050d8ef135a9a1b94731c458b4
P1er0VL <u20211a620@upc.edu.pe>
2025-12-06 18:36:27 -0500

    feat(Backend): add cors config

---
+++ src/main/java/pe/com/ecocleany/ecosmart/config/CorsConfig.java
@@ -0,0 +1,33 @@
+package pe.com.ecocleany.ecosmart.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+
+        config.setAllowedOrigins(List.of(
+                "https://ecoclean-backend-platform-j5hd.onrender.com",
+                "http://localhost:4200"
+        ));
+
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
